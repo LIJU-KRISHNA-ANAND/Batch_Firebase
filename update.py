5695450ceaa0861f6eb66a5fdbfd4a3e08c294cb
LIJU-KRISHNA-ANAND <118753816+LIJU-KRISHNA-ANAND@users.noreply.github.com>
2025-04-24 11:14:19 +0530

    Add files via upload

--- update.py
+++ update.py
@@ -1,22 +1,31 @@
+import os
+import json
+import base64
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Initialize Firebase Admin
-cred = credentials.Certificate("keytodoapp.json")
-firebase_admin.initialize_app(cred)
+from dotenv import load_dotenv
+load_dotenv()
 
+firebase_key_base64 = os.getenv('FIREBASE_KEY_BASE64')
+
+if firebase_key_base64:
+    firebase_key_json = base64.b64decode(firebase_key_base64).decode('utf-8')
+    cred = credentials.Certificate(json.loads(firebase_key_json))
+else:
+    raise ValueError("Firebase credentials not provided!")
+
+firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 source_collection = "users"
 target_collection = "users_backup"
 
-# Fetch all docs from source
 docs = db.collection(source_collection).stream()
 
 for doc in docs:
     src_data = doc.to_dict()
 
-    # Filter only selected fields
     filtered_src = {
         "name": src_data.get("name"),
         "email": src_data.get("email"),
@@ -24,16 +33,13 @@ for doc in docs:
         "is_active": src_data.get("is_active")
     }
 
-    # Get the backup document (if exists)
     backup_ref = db.collection(target_collection).document(doc.id)
     backup_doc = backup_ref.get()
 
     if not backup_doc.exists:
-        # Create if not exists
         backup_ref.set(filtered_src)
         print(f"Created backup for: {doc.id}")
     else:
-        # Check if values differ
         current_backup = backup_doc.to_dict()
         if filtered_src != current_backup:
             backup_ref.set(filtered_src)
